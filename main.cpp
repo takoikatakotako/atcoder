880f6c82855a99743bfff9de117a0c41e0fca184
小野 純平 <snorlax.chemist.and.jazz@gmail.com>
2019-09-17 17:23:40 +0900

    ビルドが通ることを確認

--- main.cpp
+++ main.cpp
@@ -18,29 +18,8 @@ using namespace std;
 using lli = long long int;
 using ll = long long;
 
-long charCounter(string sentence, char character) {
-    return count(sentence.cbegin(), sentence.cend(), character);
-}
 
 int main() {
-
-    string S;
-    cin >> S;
-
-//    vector<bool > blocks(S.size());
-//
-//    for (int i = 0; i < S.size(): ++i) {
-//
-//    }
-
-    lli zero = charCounter(S, '1');
-    lli one = charCounter(S, '0');
-
-    if (zero > one) {
-        cout << one * 2<< endl;
-    } else {
-        cout << zero * 2<< endl;
-    }
 
     return 0;
 }
